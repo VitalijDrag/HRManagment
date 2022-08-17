6c8615c6b1ffdf7c478ca55eda40a760baf3789c
Vitalijus <vitalij.dragunovic@gmail.com>
2022-08-17 15:18:56 +0300

    initial commit

---
+++ HRManagment/src/main/java/lt/CodeAcademy/HrManagmentApplication.java
@@ -0,0 +1,13 @@
+package lt.CodeAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagmentApplication.class, args);
+	}
+
+}
---
+++ HRManagment/src/test/java/lt/CodeAcademy/HrManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.CodeAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
