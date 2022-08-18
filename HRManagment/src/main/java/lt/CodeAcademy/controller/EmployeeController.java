55ce28482e49e58665500f5ae26168874a83f763
Vitalijus <vitalij.dragunovic@gmail.com>
2022-08-18 15:57:17 +0300

    Added save employee html and post endpoint.

--- HRManagment/src/main/java/lt/CodeAcademy/controller/EmployeeController.java
+++ HRManagment/src/main/java/lt/CodeAcademy/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lt.CodeAcademy.model.Employee;
 import lt.CodeAcademy.service.EmployeeService;
@@ -25,5 +27,11 @@ public class EmployeeController {
 		Employee employee = new Employee();
 		model.addAttribute("employee", employee);
 		return "new_employee";
+	}
+	
+	@PostMapping("/saveEmployee")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		employeeService.saveEmployee(employee);
+		return "redirect:/";
 	}
 }
